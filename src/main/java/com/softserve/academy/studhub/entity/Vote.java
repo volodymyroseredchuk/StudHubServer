66f514269b2ab1fc0ba1587ecc7dec6d6a2438e1
Zap1999 <45321175+Zap1999@users.noreply.github.com>
2019-06-08 18:32:01 +0300

    Vote entity addded

---
+++ src/main/java/com/softserve/academy/studhub/entity/Vote.java
@@ -0,0 +1,40 @@
+package com.softserve.academy.studhub.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "votes")
+public class Vote {
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "value")
+    @NotNull
+    private int value;
+
+    @Column(name = "user_id")
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @Column(name = "answer_id")
+    @ManyToOne
+    @JoinColumn(name = "answer_id", referencedColumnName = "id")
+    private Answer answer;
+
+    @Column(name = "feedback_id")
+    @ManyToOne
+    @JoinColumn(name = "feedback_id", referencedColumnName = "id")
+    private Feedback feedback;
+
+}
