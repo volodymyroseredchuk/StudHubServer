ff25fde1a06128a8cf2df2f1c1c71de50e5a83bd
OlhaLozinska <olja.lviv133@gmail.com>
2019-06-08 20:55:06 +0300

    changed id type to Integer

--- src/main/java/com/softserve/academy/studhub/entity/FeedBack.java
+++ src/main/java/com/softserve/academy/studhub/entity/FeedBack.java
@@ -19,7 +19,7 @@ public class FeedBack {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private int id;
+    private Integer id;
 
     @Column(name = "body")
     private String body;
