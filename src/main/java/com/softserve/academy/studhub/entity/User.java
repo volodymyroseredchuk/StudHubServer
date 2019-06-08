1e1c2f707e64835923c0659c465cae550217aaa2
DaNkOLULzz <danylolototskyi@gmail.com>
2019-06-08 20:27:55 +0300

    change id type to Integer

--- src/main/java/com/softserve/academy/studhub/entity/User.java
+++ src/main/java/com/softserve/academy/studhub/entity/User.java
@@ -18,7 +18,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private int id;
+    private Integer id;
 
     @NonNull
     @EqualsAndHashCode.Exclude
