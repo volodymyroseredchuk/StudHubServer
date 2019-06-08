7e5ad71ddcb661d2562e43b8c3960328bcf8790b
Zap1999 <45321175+Zap1999@users.noreply.github.com>
2019-06-08 18:41:13 +0300

    hash and equals added

--- src/main/java/com/softserve/academy/studhub/entity/Vote.java
+++ src/main/java/com/softserve/academy/studhub/entity/Vote.java
@@ -1,5 +1,6 @@
 package com.softserve.academy.studhub.entity;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,28 +11,35 @@ import javax.validation.constraints.NotNull;
 @Getter
 @Setter
 @NoArgsConstructor
+@EqualsAndHashCode
 @Entity
 @Table(name = "votes")
 public class Vote {
+
+    @EqualsAndHashCode.Exclude
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
 
+    @EqualsAndHashCode.Exclude
     @Column(name = "value")
     @NotNull
     private int value;
 
+    @EqualsAndHashCode.Exclude
     @Column(name = "user_id")
     @ManyToOne
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
 
+    @EqualsAndHashCode.Exclude
     @Column(name = "answer_id")
     @ManyToOne
     @JoinColumn(name = "answer_id", referencedColumnName = "id")
     private Answer answer;
 
+    @EqualsAndHashCode.Exclude
     @Column(name = "feedback_id")
     @ManyToOne
     @JoinColumn(name = "feedback_id", referencedColumnName = "id")
