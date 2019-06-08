3eb9dd3502bb0e749f556e11e41992f05eadbf47
Volodymyr Oseredchuk <oseredchuk.v@gmail.com>
2019-06-08 23:45:20 +0300

    Add entity class Tag

---
+++ src/main/java/com/softserve/academy/studhub/entity/Tag.java
@@ -0,0 +1,24 @@
+package com.softserve.academy.studhub.entity;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "tags")
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+}
