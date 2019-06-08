16d5aba9bebc22ce8951215c70ac0e98b38bafa1
OlhaLozinska <olja.lviv133@gmail.com>
2019-06-08 19:17:01 +0300

    added FeedBack entity

---
+++ src/main/java/com/softserve/academy/studhub/entity/FeedBack.java
@@ -0,0 +1,40 @@
+package com.softserve.academy.studhub.entity;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@EqualsAndHashCode
+@Table(name = "feedback")
+public class FeedBack {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "body")
+    private String body;
+
+    @Column(name = "mark")
+    private int mark;
+
+    @Column(name = "teacher_id")
+    @ManyToOne
+    @JoinColumn(name = "teacher_id", referencedColumnName = "id")
+    private Teacher teacher;
+
+    @Column(name = "univer_id")
+    @ManyToOne
+    @JoinColumn(name = "univer_id", referencedColumnName = "id")
+    private Univer univer;
+}
+
