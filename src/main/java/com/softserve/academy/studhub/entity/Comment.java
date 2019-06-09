fd7113c729ddb72e62da18d708eb9fb47a356eb7
Olena <diru4ova@gmail.com>
2019-06-09 13:30:55 +0300

    Question & Comment entities created

---
+++ src/main/java/com/softserve/academy/studhub/entity/Comment.java
@@ -0,0 +1,39 @@
+package com.softserve.academy.studhub.entity;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(exclude = {"body", "rate", "answer", "user"})
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(columnDefinition = "TEXT", name = "body")
+    private String body;
+    @Transient
+    private Integer rate;
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+    @Column(name = "modified_date")
+    private LocalDateTime modefiedDate;
+    @ManyToOne
+    @JoinColumn(name = "answer_id", referencedColumnName = "id")
+    private Answer answer;
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+
+}
