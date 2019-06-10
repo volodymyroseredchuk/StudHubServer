077ad948ace44e3160d412806dc49b70fceb01a7
Volodymyr Oseredchuk <oseredchuk.v@gmail.com>
2019-06-10 20:26:31 +0300

    Configured auto running scripts for db creation and initializing with test data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,332 @@
+-- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
+--
+-- Host: localhost    Database: studhub
+-- ------------------------------------------------------
+-- Server version	8.0.16
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8mb4 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `answers`
+--
+
+DROP TABLE IF EXISTS `answers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `answers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `approved` bit(1) DEFAULT NULL,
+  `body` text,
+  `creation_date` datetime DEFAULT NULL,
+  `question_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK3erw1a3t0r78st8ty27x6v3g1` (`question_id`),
+  KEY `FK5bp3d5loftq2vjn683ephn75a` (`user_id`),
+  CONSTRAINT `FK3erw1a3t0r78st8ty27x6v3g1` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`),
+  CONSTRAINT `FK5bp3d5loftq2vjn683ephn75a` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `answers`
+--
+
+LOCK TABLES `answers` WRITE;
+/*!40000 ALTER TABLE `answers` DISABLE KEYS */;
+INSERT INTO `answers` VALUES (1,_binary '','test','2019-06-03 00:00:00',1,2),(2,_binary '\0','Prepare yourself for being in the army!','2019-06-03 12:59:00',1,3);
+/*!40000 ALTER TABLE `answers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `body` text,
+  `creation_date` datetime DEFAULT NULL,
+  `modified_date` datetime DEFAULT NULL,
+  `answer_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKoiwlwqmu9qm0tjnafxqr20rd8` (`answer_id`),
+  KEY `FK8omq0tc18jd43bu5tjh6jvraq` (`user_id`),
+  CONSTRAINT `FK8omq0tc18jd43bu5tjh6jvraq` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `FKoiwlwqmu9qm0tjnafxqr20rd8` FOREIGN KEY (`answer_id`) REFERENCES `answers` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comments`
+--
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+INSERT INTO `comments` VALUES (1,'WTF?','2019-06-10 00:00:00',NULL,1,4);
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `feedbacks`
+--
+
+DROP TABLE IF EXISTS `feedbacks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `feedbacks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `body` text,
+  `mark` int(11) DEFAULT NULL,
+  `teacher_id` int(11) DEFAULT NULL,
+  `university_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKkmb4xm2mu42sp99hn4o0v3knf` (`teacher_id`),
+  KEY `FKf4nldstwbyijft4d6g1tyvqfx` (`university_id`),
+  CONSTRAINT `FKf4nldstwbyijft4d6g1tyvqfx` FOREIGN KEY (`university_id`) REFERENCES `universities` (`id`),
+  CONSTRAINT `FKkmb4xm2mu42sp99hn4o0v3knf` FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `feedbacks`
+--
+
+LOCK TABLES `feedbacks` WRITE;
+/*!40000 ALTER TABLE `feedbacks` DISABLE KEYS */;
+INSERT INTO `feedbacks` VALUES (1,'The worst corrupted university in the whole universe',1,NULL,1),(2,'Not as bad as they say',3,NULL,1),(3,'Totally liked that!',5,NULL,1),(4,'So far so good.',4,NULL,2),(5,'To pass exam you need only 0.5 bottle of vodka. The funniest learning experience ever)',5,NULL,3),(6,'Don`t know what to say',2,NULL,4),(7,'As a lecturer he is the worst, but as a person he\'s good',3,1,NULL),(8,'bad_bad_bad_bad_bad_bad_bad bad_bad_bad_bad_bad_bad_bad_bad_bad',1,2,NULL),(9,'good',5,2,NULL),(10,'Sample text',4,4,NULL),(11,'Petuch',3,6,NULL);
+/*!40000 ALTER TABLE `feedbacks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `questions`
+--
+
+DROP TABLE IF EXISTS `questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `questions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `body` text,
+  `creation_date` datetime DEFAULT NULL,
+  `modified_date` datetime DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKjoo8hp6d3gfwctr68dl2iaemj` (`user_id`),
+  CONSTRAINT `FKjoo8hp6d3gfwctr68dl2iaemj` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `questions`
+--
+
+LOCK TABLES `questions` WRITE;
+/*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+INSERT INTO `questions` VALUES (1,'Hi! I am applied math student, but i don\'t know math. Completely. I even don\'t know how to multiply numbes. Please help! ','2019-06-01 00:00:00',NULL,'How to math?',1),(2,'Hello! Please help me writing diploma. My theme is the philosophy of middle ages. What philosophers ere popular that days?','2019-06-01 00:00:00',NULL,'Philosophy diploma',4);
+/*!40000 ALTER TABLE `questions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `questions_tags`
+--
+
+DROP TABLE IF EXISTS `questions_tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `questions_tags` (
+  `question_id` int(11) NOT NULL,
+  `tag_id` int(11) NOT NULL,
+  KEY `FK7yq8xf1pqv8katljm8v8j8w3c` (`tag_id`),
+  KEY `FK4u5xv906wfevngoe973bec6u0` (`question_id`),
+  CONSTRAINT `FK4u5xv906wfevngoe973bec6u0` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`),
+  CONSTRAINT `FK7yq8xf1pqv8katljm8v8j8w3c` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `questions_tags`
+--
+
+LOCK TABLES `questions_tags` WRITE;
+/*!40000 ALTER TABLE `questions_tags` DISABLE KEYS */;
+INSERT INTO `questions_tags` VALUES (1,1),(2,4),(2,5);
+/*!40000 ALTER TABLE `questions_tags` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tags`
+--
+
+DROP TABLE IF EXISTS `tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `tags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_t48xdq560gs3gap9g7jg36kgc` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tags`
+--
+
+LOCK TABLES `tags` WRITE;
+/*!40000 ALTER TABLE `tags` DISABLE KEYS */;
+INSERT INTO `tags` VALUES (2,'Calculus'),(5,'Diploma'),(3,'Economics'),(1,'Math'),(4,'Philosophy'),(6,'Physics');
+/*!40000 ALTER TABLE `tags` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `teachers`
+--
+
+DROP TABLE IF EXISTS `teachers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `teachers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `creation_date` datetime DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `image_url` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `modified_date` datetime DEFAULT NULL,
+  `university_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKed4htex35qkosty8lbt8g37vb` (`university_id`),
+  CONSTRAINT `FKed4htex35qkosty8lbt8g37vb` FOREIGN KEY (`university_id`) REFERENCES `universities` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `teachers`
+--
+
+LOCK TABLES `teachers` WRITE;
+/*!40000 ALTER TABLE `teachers` DISABLE KEYS */;
+INSERT INTO `teachers` VALUES (1,'2019-06-02 00:00:00','Bogdan',NULL,'Gnativ',NULL,1),(2,'2019-06-02 00:00:00','Petro',NULL,'Kostrobiy',NULL,1),(3,'2019-06-02 00:00:00','Petro',NULL,'Topulko',NULL,1),(4,'2019-06-02 00:00:00','Roman',NULL,'Kalyna',NULL,2),(5,'2019-06-02 00:00:00','Olexiy',NULL,'Pidlisnyi',NULL,2),(6,'2019-06-02 00:00:00','Oleg',NULL,'Petuchello',NULL,3),(7,'2019-06-02 00:00:00','Igor',NULL,'Kolega',NULL,4);
+/*!40000 ALTER TABLE `teachers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `universities`
+--
+
+DROP TABLE IF EXISTS `universities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `universities` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `city` varchar(255) DEFAULT NULL,
+  `creation_date` datetime DEFAULT NULL,
+  `image_url` varchar(255) DEFAULT NULL,
+  `modified_date` datetime DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `universities`
+--
+
+LOCK TABLES `universities` WRITE;
+/*!40000 ALTER TABLE `universities` DISABLE KEYS */;
+INSERT INTO `universities` VALUES (1,'Lviv','2019-06-01 00:00:00',NULL,NULL,'Lviv Polytechnic National University'),(2,'Lviv','2019-06-01 00:00:00',NULL,NULL,'Ivan Franko Lviv National University'),(3,'Lviv','2019-06-01 00:00:00',NULL,NULL,'Bursa'),(4,'Kyiv','2019-06-01 00:00:00',NULL,NULL,'Kyiv Polytechnic University');
+/*!40000 ALTER TABLE `universities` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `creation_date` date NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `image_url` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `university_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_6dotkott2kjsp8vw4d0m25fb7` (`email`),
+  UNIQUE KEY `UK_k8d0f2n7n88w1a16yhua64onx` (`user_name`),
+  KEY `FKm6cuniuttvvmhstb2s32jsotf` (`university_id`),
+  CONSTRAINT `FKm6cuniuttvvmhstb2s32jsotf` FOREIGN KEY (`university_id`) REFERENCES `universities` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'2019-05-05','tarasgl@gmail.com','Taras',NULL,'Hlukhovetskyi','123','USER','tarasgl',NULL),(2,'2019-06-05','sample@gmail.com','Olha',NULL,'Lozinska','123','USER','olozh',NULL),(3,'2019-06-07','admin@gmail.com','Admin',NULL,'Admin','admin','ADMIN','admin',NULL),(4,'2019-06-08','sample2@gmail.com','Andrii',NULL,'Vashchenok','123','USER','avash',NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `votes`
+--
+
+DROP TABLE IF EXISTS `votes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `votes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `value` int(11) NOT NULL,
+  `answer_id` int(11) DEFAULT NULL,
+  `feedback_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK153p8dvxvoas3c6kiwxjjxkrd` (`answer_id`),
+  KEY `FKruiggkp5t2k72ks0ojsyaj867` (`feedback_id`),
+  KEY `FKli4uj3ic2vypf5pialchj925e` (`user_id`),
+  CONSTRAINT `FK153p8dvxvoas3c6kiwxjjxkrd` FOREIGN KEY (`answer_id`) REFERENCES `answers` (`id`),
+  CONSTRAINT `FKli4uj3ic2vypf5pialchj925e` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `FKruiggkp5t2k72ks0ojsyaj867` FOREIGN KEY (`feedback_id`) REFERENCES `feedbacks` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `votes`
+--
+
+LOCK TABLES `votes` WRITE;
+/*!40000 ALTER TABLE `votes` DISABLE KEYS */;
+INSERT INTO `votes` VALUES (1,1,2,NULL,2),(2,-1,1,NULL,2),(3,1,1,NULL,3),(4,-1,1,NULL,4);
+/*!40000 ALTER TABLE `votes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-06-10 18:37:08
