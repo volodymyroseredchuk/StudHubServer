e84bdc64cfe99f7f89e32a392a3181051cd2938c
Zap1999 <45321175+Zap1999@users.noreply.github.com>
2019-06-12 16:25:27 +0300

    swagger imported, security permission chaged

--- src/main/java/com/softserve/academy/studhub/config/SwaggerConfig.java
+++ src/main/java/com/softserve/academy/studhub/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.softserve.academy.studhub;
+package com.softserve.academy.studhub.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/softserve/academy/studhub/controller/HelloController.java
+++ src/main/java/com/softserve/academy/studhub/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping
+    @GetMapping(value = "home")
     public String index() {
         return "Greetings from StudHub!";
     }
