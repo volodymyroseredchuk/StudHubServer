504dd5ce4f57e54ac48dba28a9346adf1a93c20d
Zap1999 <45321175+Zap1999@users.noreply.github.com>
2019-06-12 16:29:13 +0300

    excess method removed

--- src/main/java/com/softserve/academy/studhub/repository/VoteRepository.java
+++ src/main/java/com/softserve/academy/studhub/repository/VoteRepository.java
@@ -7,12 +7,8 @@ import com.softserve.academy.studhub.entity.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface VoteRepository extends JpaRepository<Vote, Integer> {
-
-    //TODO: remove
-    Optional<Vote> findById(Integer id);
 
     List<Vote> findByUser(User user);
     List<Vote> findByFeedback(Feedback feedback);
