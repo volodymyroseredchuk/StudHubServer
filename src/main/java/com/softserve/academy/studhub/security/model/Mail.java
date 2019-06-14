0dc71444a3d14ef9b417a8fbea07b25e2f7da679
Rostyk Hlynka <hlynka1608@gmail.com>
2019-06-14 21:48:19 +0300

    models for restoring password

---
+++ src/main/java/com/softserve/academy/studhub/security/model/Mail.java
@@ -0,0 +1,14 @@
+package com.softserve.academy.studhub.security.model;
+
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+public class Mail {
+
+    private String from;
+    private String to;
+    private String subject;
+    private Map<String, Object> model;
+}
