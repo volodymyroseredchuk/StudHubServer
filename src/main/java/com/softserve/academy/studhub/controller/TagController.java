cf0b505fe236536dbce35009ac7c062b5d5bf76e
Volodymyr Oseredchuk <oseredchuk.v@gmail.com>
2019-06-14 15:54:01 +0300

    Fix bug with no connection due to CORS

--- src/main/java/com/softserve/academy/studhub/controller/TagController.java
+++ src/main/java/com/softserve/academy/studhub/controller/TagController.java
@@ -3,13 +3,13 @@ package com.softserve.academy.studhub.controller;
 import com.softserve.academy.studhub.dto.TagsDTO;
 import com.softserve.academy.studhub.service.TagService;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/tags")
 public class TagController {
@@ -22,9 +22,6 @@ public class TagController {
 
     @GetMapping
     public ResponseEntity<TagsDTO> getAllTagsSortedWithPagination(Pageable pageable) {
-        HttpHeaders respHeaders = new HttpHeaders();
-        respHeaders.set("Access-Control-Allow-Origin", "*");
-
-        return ResponseEntity.ok().headers(respHeaders).body(tagService.findAllSorted(pageable));
+        return ResponseEntity.ok().body(tagService.findAllSorted(pageable));
     }
 }
