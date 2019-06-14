84a41a6bb2832efc34b801b45d6518cf49052451
Rostyk Hlynka <hlynka1608@gmail.com>
2019-06-14 21:49:18 +0300

    provided sending mail with reset link

---
+++ src/main/java/com/softserve/academy/studhub/service/EmailService.java
@@ -0,0 +1,53 @@
+package com.softserve.academy.studhub.service;
+
+import com.softserve.academy.studhub.security.model.Mail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import javax.mail.internet.MimeMessage;
+import java.nio.charset.StandardCharsets;
+
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+
+    public void sendEmail(Mail mail) {
+
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message,
+                    MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                    StandardCharsets.UTF_8.name());
+
+            Context context = new Context();
+            context.setVariables(mail.getModel());
+            String html = templateEngine.process("email/email-template", context);
+
+            helper.setFrom(username);
+            helper.setTo(mail.getTo());
+            helper.setText(html, true);
+            helper.setSubject(mail.getSubject());
+
+            mailSender.send(message);
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+}
