e3e4a33942672e2480b3580006253752282c7ea6
Rostyk Hlynka <hlynka1608@gmail.com>
2019-06-16 14:46:47 +0300

    add moderator role

--- src/main/java/com/softserve/academy/studhub/service/RoleService.java
+++ src/main/java/com/softserve/academy/studhub/service/RoleService.java
@@ -3,9 +3,7 @@ package com.softserve.academy.studhub.service;
 import com.softserve.academy.studhub.entity.Role;
 import com.softserve.academy.studhub.entity.enums.RoleName;
 
-import java.util.Optional;
-
 public interface RoleService {
 
-    Optional<Role> findByName(RoleName roleName);
+    Role findByName(RoleName roleName);
 }
