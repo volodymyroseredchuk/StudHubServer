861a299db3d43ece4e6cc38b1f0d850fb8232032
Rostyk Hlynka <hlynka1608@gmail.com>
2019-06-22 15:48:53 +0300

    fix creating email

--- src/main/java/com/softserve/academy/studhub/service/EmailService.java
+++ src/main/java/com/softserve/academy/studhub/service/EmailService.java
@@ -1,8 +1,9 @@
 package com.softserve.academy.studhub.service;
 
-import com.softserve.academy.studhub.security.model.Mail;
+import com.softserve.academy.studhub.entity.User;
+import com.softserve.academy.studhub.security.model.PasswordResetToken;
 
 public interface EmailService {
 
-    void sendEmail(Mail mail);
+    void sendResetPasswordEmail(User receiver, PasswordResetToken token);
 }
