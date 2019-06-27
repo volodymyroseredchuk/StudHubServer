ea26c98604d989ccc5326a38e6fbb92e9b8e6901
Volodymyr Oseredchuk <oseredchuk.v@gmail.com>
2019-06-27 21:17:47 +0300

    Create proposal service implementation

--- src/main/java/com/softserve/academy/studhub/config/Message.java
+++ src/main/java/com/softserve/academy/studhub/config/Message.java
@@ -2,7 +2,9 @@ package com.softserve.academy.studhub.config;
 
 public class Message {
 
-    public static final String TASK_DELETED = "Task was deleted successfully";
+    public static final String TASK_DELETED = "Task was successfully deleted";
 
     public static final String TASK_NOT_DELETED = "This task already has proposals and can not be deleted";
+
+    public static final String PROPOSAL_DELETED = "Proposal was successfully deleted";
 }
