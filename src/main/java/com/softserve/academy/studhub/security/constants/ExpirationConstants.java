6510f10a6437364a5fd0bf1efec89659592fb438
Rostyk Hlynka <hlynka1608@gmail.com>
2019-06-27 23:44:26 +0300

    add constants

---
+++ src/main/java/com/softserve/academy/studhub/security/constants/ExpirationConstants.java
@@ -0,0 +1,7 @@
+package com.softserve.academy.studhub.security.constants;
+
+public class ExpirationConstants {
+
+    public static final int CONFIRM_TOKEN_EXPIRATION_IN_MINUTES = 30;
+    public static final int PASSWORD_RESET_TOKEN_EXPIRATION_IN_MINUTES = 30;
+}
