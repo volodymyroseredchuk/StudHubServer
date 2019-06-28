e69755c37f0db51d184b011f257a4e4ef813bbed
Volodymyr Oseredchuk <oseredchuk.v@gmail.com>
2019-06-28 09:53:10 +0300

    Create method in controller to get task by id and corresponding DTO

--- src/main/java/com/softserve/academy/studhub/controller/TaskController.java
+++ src/main/java/com/softserve/academy/studhub/controller/TaskController.java
@@ -1,10 +1,6 @@
 package com.softserve.academy.studhub.controller;
 
-import com.softserve.academy.studhub.dto.QuestionForListDTO;
-import com.softserve.academy.studhub.dto.QuestionPaginatedDTO;
-import com.softserve.academy.studhub.dto.TaskForListDTO;
-import com.softserve.academy.studhub.dto.TaskPaginatedDTO;
-import com.softserve.academy.studhub.entity.Question;
+import com.softserve.academy.studhub.dto.*;
 import com.softserve.academy.studhub.entity.Task;
 import com.softserve.academy.studhub.service.TaskService;
 import lombok.AllArgsConstructor;
@@ -13,10 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -40,5 +33,13 @@ public class TaskController {
             .collect(Collectors.toList());
 
         return ResponseEntity.ok().body(new TaskPaginatedDTO(taskForListDTOs, taskPage.getTotalElements()));
+    }
+
+    @GetMapping("/{taskId}")
+    @PreAuthorize("permitAll()")
+    public ResponseEntity<TaskDTO> getTask(@PathVariable Integer taskId) {
+        Task task = taskService.findById(taskId);
+
+        return ResponseEntity.ok().body(modelMapper.map(task, TaskDTO.class));
     }
 }
---
+++ src/main/java/com/softserve/academy/studhub/dto/TaskDTO.java
@@ -0,0 +1,27 @@
+package com.softserve.academy.studhub.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class TaskDTO {
+
+    private Integer id;
+
+    private String title;
+
+    private LocalDateTime creationDate;
+
+    private LocalDateTime modifiedDate;
+
+    private LocalDateTime deadlineDate;
+
+    private Integer expectedPrice;
+
+    private UserGeneralInfoDTO user;
+}
