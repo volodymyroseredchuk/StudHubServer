9f9eb49f3f1c257cd9950500e446c24fea8c3e93
DaNkOLULzz <danylolototskyi@gmail.com>
2019-07-16 20:00:05 +0300

    add UrlDTO and fix uploadFile method

--- src/main/java/com/softserve/academy/studhub/controller/FileController.java
+++ src/main/java/com/softserve/academy/studhub/controller/FileController.java
@@ -1,18 +1,17 @@
 package com.softserve.academy.studhub.controller;
 
 
+import com.softserve.academy.studhub.dto.UrlDTO;
 import com.softserve.academy.studhub.service.FileService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+@CrossOrigin
 @RestController
 @AllArgsConstructor
 @RequestMapping("/files")
@@ -22,7 +21,10 @@ public class FileController {
 
     @PostMapping("/upload")
     @PreAuthorize("permitAll()")
-    ResponseEntity<String> uploadFile(@RequestParam MultipartFile multipartFile) throws IOException {
-        return ResponseEntity.ok(fileService.uploadFile(multipartFile));
+    ResponseEntity<UrlDTO> uploadFile(@RequestParam MultipartFile multipartFile) throws IOException {
+
+        UrlDTO result = new UrlDTO(fileService.uploadFile(multipartFile));
+
+        return ResponseEntity.ok(result);
     }
 }
---
+++ src/main/java/com/softserve/academy/studhub/dto/UrlDTO.java
@@ -0,0 +1,13 @@
+package com.softserve.academy.studhub.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UrlDTO {
+
+    private String message;
+}
