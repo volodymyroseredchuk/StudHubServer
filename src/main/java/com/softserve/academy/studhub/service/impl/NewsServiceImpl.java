fdb86226b19b7dd7eefed768d6fb020f71ec1877
tarasgl <tarasgl@gmail.com>
2019-07-31 13:40:00 +0300

    reproducing bug with invalid format

--- src/main/java/com/softserve/academy/studhub/service/impl/NewsServiceImpl.java
+++ src/main/java/com/softserve/academy/studhub/service/impl/NewsServiceImpl.java
@@ -75,7 +75,7 @@ public class NewsServiceImpl implements NewsService {
             if (!existByUrl(link)) {
                 String title = parser.parseTitle(link);
                 String body = parser.parseBody(link);
-                log.debug("News body " + body);
+                log.trace("News body " + body);
                 News news = new News();
                 news.setTitle(title);
                 news.setBody(body);
